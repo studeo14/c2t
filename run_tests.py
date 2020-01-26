93ded512f06feb9c7c0a907036a5f7b16ba9c57b
Steven <sfrederiksen@vt.edu>
2020-01-26 03:00:34 -0500

    Added regression testing for c2t

---
+++ run_tests.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import os, subprocess, glob
+from pathlib import Path
+
+def get_tests():
+    (_, tests, _) = next(os.walk("./tests"))
+    return tests
+
+def run_inputs(tests):
+    for test in tests:
+        inFile = "./tests/{}/in.xmi".format(test)
+        resFile = "./tests/{}/in.project".format(test)
+        res = subprocess.run(["java", "-jar", "./cas2text.jar", "-f", inFile])
+        os.rename("./in.project", resFile)
+
+def run_compare(tests):
+    results = []
+    for test in tests:
+        inFile = "./tests/{}/in.project".format(test)
+        baseFile = "./tests/{}/out.project".format(test)
+        res = subprocess.run(["java", "-jar", "./tests/RegressionTester.jar", "-b", baseFile, "-t", inFile])
+        if res.returncode != 0:
+            os.rename("./in.diffs", "./tests/{}.diffs".format(test))
+            results.append((test, False))
+        else:
+            results.append((test, True))
+    return results
+
+def show_results(results):
+    for result in results:
+        print("{}\t:\t{}".format(result[0], "Pass" if result[1] else "Fail, see tests/{}.diffs for more info.".format(result[0])))
+
+def main():
+    tests = get_tests()
+    run_inputs(tests)
+    results = run_compare(tests)
+    show_results(results)
+
+if __name__ == "__main__":
+    main()
